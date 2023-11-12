33ba69b0925f241250f24b85777d8e87fcb98a40
okancinar <okancinar10189@gmail.com>
2023-11-12 18:54:18 +0300

    Daire Alan Odev Eklendi

---
+++ src/TemelKavramlarveDegiskenler/DaireAlanCevreHesaplama.java
@@ -0,0 +1,30 @@
+package TemelKavramlarveDegiskenler;
+
+import java.util.Scanner;
+
+public class DaireAlanCevreHesaplama {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen dairenin yarıçapını girin: ");
+        int r = input.nextInt();
+
+        /*
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Alan: " + alan + "\nÇevre: " + cevre);
+        */
+
+
+        // Açı ile hesaplama:
+
+        System.out.print("Merkez Açı: ");
+        int a = input.nextInt();
+
+        double dilimAlan = (pi * (r*r) * a) / 360;
+
+        System.out.println("Daire dilimi alanı: " + dilimAlan);
+
+    }
+}
