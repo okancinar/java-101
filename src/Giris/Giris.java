afdc3b8166c02031791d30584e123e27cb2c1789
okancinar <okancinar10189@gmail.com>
2023-11-12 18:02:52 +0300

    Odev Eklendi

---
+++ src/Giris/Giris.java
@@ -0,0 +1,9 @@
+package Giris;
+
+public class Giris {
+    public static void main(String[] args) {
+        byte b = 0;
+        int a = b++;
+        System.out.print(a);
+    }
+}
---
+++ src/TemelKavramlarveDegiskenler/NotOrt.java
@@ -0,0 +1,40 @@
+package TemelKavramlarveDegiskenler;
+
+import java.util.Scanner;
+
+public class NotOrt {
+    public static void main(String[] args) {
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen Matematik Notunuzu Girin: ");
+        mat = inp.nextInt();
+
+        System.out.print("Lütfen Fizik Notunuzu Girin: ");
+        fizik = inp.nextInt();
+
+        System.out.print("Lütfen Kimya Notunuzu Girin: ");
+        kimya = inp.nextInt();
+
+        System.out.print("Lütfen Türkçe Notunuzu Girin: ");
+        turkce = inp.nextInt();
+
+        System.out.print("Lütfen Tarih Notunuzu Girin: ");
+        tarih = inp.nextInt();
+
+        System.out.print("Lütfen Müzik Notunuzu Girin: ");
+        muzik = inp.nextInt();
+
+        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
+        double ortalama = toplam / 6.0;
+
+        System.out.println("Notunuz: " + ortalama);
+
+        String durum = (ortalama > 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(durum);
+
+
+    }
+}
