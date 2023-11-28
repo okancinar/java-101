5bec2c2901e7296245d8479f2ee16347fdccd5d1
okancinar <okancinar10189@gmail.com>
2023-11-28 18:33:40 +0300

    Odev Eklendi

---
+++ src/fibonacci/Main.java
@@ -0,0 +1,24 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n1 = 0, n2 = 1;
+        int toplam = n1 + n2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fibonacci sayı adeti giriniz:  ");
+        int value = input.nextInt();
+
+        System.out.print(n1 + " " + n2 + " ");
+        while (value > 0)
+        {
+            System.out.print(toplam + " ");
+            value--;
+            n1 = n2; n2 = toplam; toplam= n1 + n2;
+        }
+    }
+}
