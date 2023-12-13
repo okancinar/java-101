b70130205741054c03881f2ca569d42b8fc01af2
okancinar <okancinar10189@gmail.com>
2023-12-14 00:28:49 +0300

    Odev Eklendi

---
+++ src/salarycalculator/Main.java
@@ -0,0 +1,10 @@
+package salarycalculator;
+
+public class Main {
+    public static void main(String[] args){
+
+        Employee worker = new Employee("Okan",50000,60,2020);
+
+        worker.infoWorker();
+    }
+}
